d7a1a99ecfacfdaea3c44f3643bf6d68964d7d02
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-11 22:30:46 +0100

    nasconde l'immagine cliccando un bottone

--- main.js
+++ main.js
@@ -5,20 +5,18 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            datapage: {
                 message: "Questo è il mio primo utilizzo di Vue :')",
                 image: "https://avatars.githubusercontent.com/u/6128107?s=200&v=4",
                 imageArray: ["img/dog1.jpg", "img/dog2.jpg", "img/dog3.jpg"],
                 hid: false
-            },
+            }
+        },
             methods: {
                 hidden(){                 
                     this.hid = true;
-                    console.log(hid);
                     console.log(this.hid);
-                }
+                    // this.imageArray.push(message);
+                },
             }
             
-        }
-    }
-}).mount('#app')
+        }).mount('#app')
