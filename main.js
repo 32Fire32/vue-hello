3e74c091eab71697646137d862c825b854d7a051
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-11 16:48:06 +0100

    stampa lista di immagini aggiunte con v-for

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 'strict mode'
 
-const { createApp} = Vue
+const { createApp } = Vue
 
 createApp({
     data() {
         return {
-            message: "Questo è il mio primo utilizzo di Vue :')",
-            image: "https://avatars.githubusercontent.com/u/6128107?s=200&v=4"
+            datapage: {
+                message: "Questo è il mio primo utilizzo di Vue :')",
+                image: "https://avatars.githubusercontent.com/u/6128107?s=200&v=4",
+                imageArray: ["img/dog1.jpg", "img/dog2.jpg", "img/dog3.jpg"],
+                hid: false
+            },
+            methods: {
+                hidden(){                 
+                    this.hid = true;
+                    console.log(hid);
+                    console.log(this.hid);
+                }
+            }
+            
         }
     }
 }).mount('#app')
